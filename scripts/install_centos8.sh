9be783ba682f26d71bf73c75163ae3c69dcf278b
Xtun <vietvukhac@gmail.com>
2022-11-03 00:24:52 +0700

    Update install_centos8.sh
    update 0.9.4

--- scripts/install_centos8.sh
+++ scripts/install_centos8.sh
@@ -15,7 +15,7 @@ install_3proxy() {
     echo "installing 3proxy"
     mkdir -p /3proxy
     cd /3proxy
-    URL="https://github.com/z3APA3A/3proxy/archive/0.9.4.tar.gz"
+    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-0.9.4
     make -f Makefile.Linux
